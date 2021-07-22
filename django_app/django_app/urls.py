a029e1bea6268ed08254fdc60d38e3ef28192f54
danil <danil>
2021-07-22 22:10:58 +0300

    initial version

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home_page, name='home'),
+    path('register/', views.register_page, name='register'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout')
 ]
---
+++ django_app/django_app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import CreateUserForm
+
+
+def home_page(request):
+    return render(request, 'base.html')
+
+
+def register_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, f'Account was created for {user}')
+
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'sign_up.html', context)
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "Username or password isn't correct")
+
+    context = {}
+    return render(request, 'sign_in.html', context)
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
