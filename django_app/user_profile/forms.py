bef6f1a2ca023def55600340c01c9056cc1b1f98
Daniel <kotljar.daniel@gmail.com>
2021-07-31 23:24:24 +0000

    user_profile_and_navbar_ui_update

--- django_app/user_profile/forms.py
+++ django_app/user_profile/forms.py
@@ -1,8 +1,22 @@
 from django.contrib.auth.models import User
 from django import forms
 
-
-class UpdateProfile(forms.ModelForm):
+class UpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['email', 'username', 'password']
+
+class UpdateNicknameForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username']
+
+class UpdateEmailForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email']
+
+class UpdatePasswordForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['password']
--- django_app/user_profile/views.py
+++ django_app/user_profile/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from .forms import UpdateProfile
+from .forms import UpdateNicknameForm, UpdateEmailForm, UpdatePasswordForm, UpdateForm
 
 
 def change_data(request):
-    form = UpdateProfile()
+    form = UpdateForm()
     if request.method == 'POST':
-        form = UpdateProfile(request.POST, instance=request.user)
-        print(form)
-        if form.is_valid():
-            form.save()
-        else:
-            form = UpdateProfile()
+        if 'Change Nickname' in request.POST:
+            form = UpdateNicknameForm(request.POST, instance=request.user)
+            if form.is_valid():
+                form.save()
+                form = UpdateForm()
+        elif 'Change Email' in request.POST:
+            form = UpdateEmailForm(request.POST, instance=request.user)
+            if form.is_valid():
+                form.save()
+                form = UpdateForm()
+        elif 'Change Password' in request.POST:
+            form = UpdatePasswordForm(request.POST, instance=request.user)
+            if form.is_valid():
+                form.save()
+                form = UpdateForm()
     context = {'form': form}
     return render(request, 'user_profile/user_profile.html', context)
