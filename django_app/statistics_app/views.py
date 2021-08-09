3f0d94a6caf6c4b414eab94517f7a70fe7fe7e0d
Andrii Kovalov <jfkwww1@gmail.com>
2021-08-09 07:34:50 +0000

    statistics url added to base template + route visible for admin only

--- django_app/statistics_app/views.py
+++ django_app/statistics_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import (
+    staff_member_required
+)
 
 
+@staff_member_required
 def stats_home(request):
     '''
     View func for '/statistics' path
