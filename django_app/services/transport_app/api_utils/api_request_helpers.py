508bb9e1b10be674d8065a7515025099bc5b696f
Andrii Kovalov <jfkwww1@gmail.com>
2021-08-25 19:17:26 +0300

    transport_app service url changes

--- django_app/services/transport_app/api_utils/api_request_helpers.py
+++ django_app/services/transport_app/api_utils/api_request_helpers.py
@@ -15,7 +15,7 @@ def create_api_call(payload, api_endpoint):
     TRANSPORT_APP_SERVICE_NAME = os.environ.get('TRANSPORT_APP_SERVICE_NAME')
     TRANSPORT_APP_SERVICE_PORT = os.environ.get('TRANSPORT_APP_SERVICE_PORT')
     TRANSPORT_API_URL = (
-        f'http://{TRANSPORT_APP_SERVICE_NAME}:{TRANSPORT_APP_SERVICE_PORT}/'
+        f'{TRANSPORT_APP_SERVICE_NAME}:{TRANSPORT_APP_SERVICE_PORT}/'
         f'{api_endpoint}'
     )
     jwt_token = create_jwt_token(payload)
